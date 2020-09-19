c69eb3aff8c74a36bf0ac6578d457c77aee908fa
fengzhizi715 <fengzhizi715@126.com>
2020-09-20 01:06:50 +0800

    add storage

---
+++ core/src/main/kotlin/cn/netdiscovery/http/core/OkHttpClientWrapper.kt
@@ -0,0 +1,10 @@
+package cn.netdiscovery.http.core
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.core.OkHttpClientWrapper
+ * @author: Tony Shen
+ * @date: 2020-09-20 01:02
+ * @version: V1.0 <描述当前版本功能>
+ */
---
+++ core/src/main/kotlin/cn/netdiscovery/http/core/storage/Storage.kt
@@ -0,0 +1,18 @@
+package cn.netdiscovery.http.core.storage
+
+import java.io.File
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.core.storage.Storage
+ * @author: Tony Shen
+ * @date: 2020-09-20 01:04
+ * @version: V1.0 <描述当前版本功能>
+ */
+interface Storage {
+
+    fun getCacheDir(): File
+
+    fun getCookieDir(): File
+}
