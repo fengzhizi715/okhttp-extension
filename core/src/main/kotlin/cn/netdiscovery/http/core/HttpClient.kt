e90f59671c297f72e62042b654417d60b90a98e1
fengzhizi715 <fengzhizi715@126.com>
2020-10-03 01:38:05 +0800

    add HttpClient

---
+++ core/src/main/kotlin/cn/netdiscovery/http/core/HttpClient.kt
@@ -0,0 +1,128 @@
+package cn.netdiscovery.http.core
+
+import cn.netdiscovery.http.core.cookie.ClientCookieHandler
+import cn.netdiscovery.http.core.storage.Storage
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import kotlin.reflect.KClass
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.core.HttpClient
+ * @author: Tony Shen
+ * @date: 2020-09-20 13:18
+ * @version: V1.0 <描述当前版本功能>
+ */
+interface HttpClient {
+
+    fun userAgent(agent: String): HttpClient
+
+    /**
+     * @see ClientCookieHandler
+     */
+    fun getClientCookieHandler(): ClientCookieHandler
+
+    /**
+     * Create post request
+     * @url - part of baseUrl
+     * @customUrl - full url which will be used instead baseUrl + url
+     * @query - will build query in url "url?param1=val1&param2..."
+     * @headers - http headers
+     *
+     * @see Call
+     *
+     * @return okhttp3.Call
+     */
+    fun get(url: String = "", customUrl: String? = null, query: Params? = null,
+            headers: Params? = null): Call
+
+    /**
+     * Create post request
+     * @url - part of baseUrl
+     * @customUrl - full url which will be used instead baseUrl + url
+     * @body - post body
+     * @headers - http headers
+     *
+     * @see Params
+     * @see Call
+     *
+     * @return okhttp3.Call
+     */
+    fun post(url: String = "", customUrl: String? = null, body: Params,
+             headers: Params? = null): Call
+
+    /**
+     * @see #post
+     * @see Params
+     * @see Call
+     */
+    fun put(url: String = "", customUrl: String? = null, body: Params,
+            headers: Params? = null): Call
+
+    /**
+     * @see #get
+     * @see Params
+     * @see Call
+     */
+    fun delete(url: String = "", customUrl: String? = null, query: Params,
+               headers: Params? = null): Call
+
+    /**
+     * @see #post
+     * @see Params
+     * @see Call
+     *
+     * @json - used instead bady in post request
+     */
+    fun jsonPost(url: String = "", customUrl: String? = null, json: String,
+                 headers: Params? = null): Call
+
+    /**
+     * @see #put and #jsonPost
+     * @see Params
+     * @see Call
+     */
+    fun jsonPut(url: String = "", customUrl: String? = null, json: String,
+                headers: Params? = null): Call
+
+    /**
+     * You can send okhttp.Request instead using defined methods
+     * RequestProcessors will be avoided
+     *
+     * @see Request
+     * @see Call
+     *
+     * @return okhttp3.Call
+     */
+    fun send(request: Request): Call
+
+    /**
+     * Same as send but request will be processed by RequestProcessors
+     *
+     * @see Request
+     * @see Call
+     * @see RequestMethod
+     */
+    fun processAndSend(request: Request.Builder): Call
+
+//    fun <T: Any> send(expectedClass: KClass<T>, request: RequestMethod<T>): Expected<T>
+//
+//    /**
+//     * @see ProcessorStore
+//     */
+//    fun getProcessorStore(): ProcessorStore
+
+    /**
+     * @see OkHttpClient
+     */
+    fun okHttpClient(): OkHttpClient
+
+    fun getUserAgent(): String?
+
+    /**
+     * @see StorageProvider
+     */
+    fun getStorageProvider(): Storage
+}
---
+++ core/src/main/kotlin/cn/netdiscovery/http/core/RequestMethod.kt
@@ -0,0 +1,63 @@
+package cn.netdiscovery.http.core
+
+import kotlin.reflect.KClass
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.core.RequestMethod
+ * @author: Tony Shen
+ * @date: 2020-10-03 01:27
+ * @version: V1.0 <描述当前版本功能>
+ */
+open class RequestMethod<T> protected constructor() {
+    var url: String? = null
+    var customUrl: String? = null
+    var dateTimePattern: String = "yyyy-MM-dd hh:mm"
+    var parallel: Boolean = false
+    var isCountableEnabled: Boolean = true
+
+    // url "localhost?myquery=myvalue"
+    var queriesParams: Params? = null
+    var queriesMap: Map<String, String>? = null
+    var queriesModel: Any? = null
+//    var queriesCountable: Countable? = null
+//    var queriesModelConverter: KClass<out RequestModelConverter>? = null
+
+    // url "localhost/{path_name}"
+    var pathParams: Params? = null
+    var pathMap: Map<String, String>? = null
+    var pathModel: Any? = null
+//    var pathCountable: Countable? = null
+//    var pathModelConverter: KClass<out RequestModelConverter>? = null
+
+    var headersParams: Params? = null
+    var headersMap: Map<String, String>? = null
+    var headersModel: Any? = null
+//    var headersCountable: Countable? = null
+//    var headersModelConverter: KClass<out RequestModelConverter>? = null
+
+//    var responseMapper: KClass<out ResponseMapper<*>>? = null
+}
+
+class GetMethod<T> : RequestMethod<T>()
+
+open class PostMethod<T> : RequestMethod<T>() {
+    var bodyParams: Params? = null
+    var bodyMap: Map<String, String>? = null
+    var bodyModel: Any? = null
+//    var bodyCountable: Countable? = null
+//    var bodyModelConverter: KClass<out RequestModelConverter>? = null
+}
+
+class PutMethod<T>: PostMethod<T>()
+
+class DeleteMethod<T>: RequestMethod<T>()
+
+open class JsonPostMethod<T> : RequestMethod<T>() {
+    var json: String? = null
+    var jsonModel: Any? = null
+//    var jsonConverter: KClass<out RequestJsonConverter>? = null
+}
+
+class JsonPutMethod<T>: JsonPostMethod<T>()
