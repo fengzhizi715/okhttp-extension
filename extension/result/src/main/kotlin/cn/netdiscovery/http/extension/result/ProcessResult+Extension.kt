827fa401502d88c5db19fd42654c089289ad90a5
fengzhizi715 <fengzhizi715@126.com>
2020-12-11 12:10:37 +0800

    update build.gradle

---
+++ extension/result/src/main/kotlin/cn/netdiscovery/http/extension/result/ProcessResult+Extension.kt
@@ -0,0 +1,17 @@
+package cn.netdiscovery.http.extension.result
+
+import cn.netdiscovery.http.core.ProcessResult
+import cn.netdiscovery.result.Result
+import cn.netdiscovery.result.resultFrom
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.extension.result.`ProcessResult+Extension`
+ * @author: Tony Shen
+ * @date: 2020-12-11 11:54
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun <T> ProcessResult<T>.getResult():Result<T,Exception> = resultFrom {
+    sync()
+}
