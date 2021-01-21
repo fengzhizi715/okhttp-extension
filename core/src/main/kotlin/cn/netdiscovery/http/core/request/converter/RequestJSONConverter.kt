2e7923e69a8baf1e95c007e3cf0707bc155c09d5
fengzhizi715 <fengzhizi715@126.com>
2021-01-21 22:01:58 +0800

    修改注释

--- core/src/main/kotlin/cn/netdiscovery/http/core/request/converter/RequestJSONConverter.kt
+++ core/src/main/kotlin/cn/netdiscovery/http/core/request/converter/RequestJSONConverter.kt
@@ -6,7 +6,7 @@ package cn.netdiscovery.http.core.request.converter
  *          cn.netdiscovery.http.core.request.converter.RequestJSONConverter
  * @author: Tony Shen
  * @date: 2020-10-08 01:11
- * @version: V1.0  将 request body 中的 json 内容转换成 String 字符串
+ * @version: V1.0  将 request body 中的对象转换成 json 格式的字符串
  */
 interface RequestJSONConverter {
 
