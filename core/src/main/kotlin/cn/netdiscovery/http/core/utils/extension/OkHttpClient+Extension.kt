df6cb283a1866715ebe9c9e13bcaf02203729936
fengzhizi715 <fengzhizi715@126.com>
2021-07-29 17:00:54 +0800

    update

--- core/src/main/kotlin/cn/netdiscovery/http/core/utils/extension/OkHttpClient+Extension.kt
+++ core/src/main/kotlin/cn/netdiscovery/http/core/utils/extension/OkHttpClient+Extension.kt
@@ -11,5 +11,3 @@ import okhttp3.*
  * @version: V1.0 <描述当前版本功能>
  */
 fun OkHttpClient.newCall(block:()->Request):Call = this.newCall(block.invoke())
-
-fun OkHttpClient.newWebSocket(block:()->Request,listener: WebSocketListener):WebSocket = this.newWebSocket(block.invoke(),listener)
