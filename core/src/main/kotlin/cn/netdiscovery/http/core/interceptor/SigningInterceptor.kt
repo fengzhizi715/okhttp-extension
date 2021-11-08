29e472bc2aa2aaa280e7065100146920d2c4cd7a
Tony Shen <fengzhizi715@126.com>
2021-11-08 14:24:56 +0800

    Update SigningInterceptor.kt

--- core/src/main/kotlin/cn/netdiscovery/http/core/interceptor/SigningInterceptor.kt
+++ core/src/main/kotlin/cn/netdiscovery/http/core/interceptor/SigningInterceptor.kt
@@ -1,7 +1,7 @@
 package cn.netdiscovery.http.core.interceptor
 
-import okhttp3.HttpUrl
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
 /**
@@ -13,14 +13,19 @@ import okhttp3.Response
  * @date: 2021/11/8 11:14 上午
  * @version: V1.0 <描述当前版本功能>
  */
-class SigningInterceptor(private val parameterName: String, private val signer: HttpUrl.() -> String) : Interceptor {
+class SigningInterceptor(private val parameterName: String,
+                         private val extraMap: Map<String, String>? =null,
+                         private val signer: Request.() -> String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response =
 
         with(chain.request()) {
-            val signedKey = signer(url)
+            val signedKey = signer(this)
 
             val requestUrl = with(url.newBuilder()) {
                 addQueryParameter(parameterName, signedKey)
+                extraMap?.let {
+                    it.forEach { (k, v) ->  addQueryParameter(k, v)}
+                }
                 build()
             }
 
