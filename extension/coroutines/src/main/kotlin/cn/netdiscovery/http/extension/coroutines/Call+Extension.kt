93177ccdfadd2b1d9723f0836f98ff9fa3b6e256
Tony Shen <fengzhizi715@126.com>
2021-11-09 01:54:21 +0800

    update

---
+++ extension/coroutines/src/main/kotlin/cn/netdiscovery/http/extension/coroutines/Call+Extension.kt
@@ -0,0 +1,35 @@
+package cn.netdiscovery.http.extension.coroutines
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.extension.coroutines.`Call+Extension`
+ * @author: Tony Shen
+ * @date: 2021-11-09 01:47
+ * @version: V1.0 <描述当前版本功能>
+ */
+suspend fun Call.Factory.suspendCall(request: Request): Response =
+    suspendCancellableCoroutine { cont ->
+        val call = newCall(request)
+        cont.invokeOnCancellation {
+            call.cancel()
+        }
+        call.enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                cont.resume(response)
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                cont.resumeWithException(e)
+            }
+        })
+    }
