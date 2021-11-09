cbaca8fd796d27d9ca9ec3f1c554b2dae7175d82
Tony Shen <fengzhizi715@126.com>
2021-11-09 16:31:19 +0800

    Update Call+Extension.kt

--- extension/coroutines/src/main/kotlin/cn/netdiscovery/http/extension/coroutines/Call+Extension.kt
+++ extension/coroutines/src/main/kotlin/cn/netdiscovery/http/extension/coroutines/Call+Extension.kt
@@ -3,7 +3,6 @@ package cn.netdiscovery.http.extension.coroutines
 import kotlinx.coroutines.suspendCancellableCoroutine
 import okhttp3.Call
 import okhttp3.Callback
-import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
 import kotlin.coroutines.resume
@@ -17,14 +16,14 @@ import kotlin.coroutines.resumeWithException
  * @date: 2021-11-09 01:47
  * @version: V1.0 <描述当前版本功能>
  */
-suspend fun Call.Factory.suspendCall(request: Request): Response =
+suspend fun Call.suspendCall(): Response =
     suspendCancellableCoroutine { cont ->
-        val call = newCall(request)
         cont.invokeOnCancellation {
-            call.cancel()
+            this.cancel()
         }
-        call.enqueue(object : Callback {
+        this.enqueue(object : Callback {
             override fun onResponse(call: Call, response: Response) {
+                println("Thread Name:"+Thread.currentThread().name)
                 cont.resume(response)
             }
 
