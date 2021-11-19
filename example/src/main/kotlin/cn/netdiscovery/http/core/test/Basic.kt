9a19d25c70ea06ce408adc8402df8cc77c96ca08
Tony Shen <fengzhizi715@126.com>
2021-11-19 14:17:53 +0800

    Create Basic.kt

---
+++ example/src/main/kotlin/cn/netdiscovery/http/core/test/Basic.kt
@@ -0,0 +1,27 @@
+package cn.netdiscovery.http.core.test
+
+import cn.netdiscovery.http.core.utils.extension.httpGet
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.core.test.Basic
+ * @author: Tony Shen
+ * @date: 2021/11/19 1:47 下午
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun main() {
+
+    "https://baidu.com".httpGet().use {
+        println(it)
+    }
+
+//    runBlocking {
+//        "https://baidu.com".asyncGet()
+//            .await()
+//            .use {
+//                println(it)
+//        }
+//    }
+
+}
