3df04b394a44f54184a7adc2131f4a4646af29c8
Tony Shen <fengzhizi715@126.com>
2021-11-30 11:24:09 +0800

    Create StompException.kt

---
+++ core/src/main/kotlin/cn/netdiscovery/http/core/exception/StompException.kt
@@ -0,0 +1,11 @@
+package cn.netdiscovery.http.core.exception
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.core.exception.StompException
+ * @author: Tony Shen
+ * @date: 2021/11/30 11:14 上午
+ * @version: V1.0 <描述当前版本功能>
+ */
+class StompException(msg: String): RuntimeException(msg)
