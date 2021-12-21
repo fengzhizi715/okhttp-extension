71e77307f2a3458e95b539f56059344c5cf2ce20
Tony Shen <fengzhizi715@126.com>
2021-12-22 01:06:58 +0800

    update

---
+++ extension/rxjava3/src/main/kotlin/cn/netdiscovery/http/extension/rxjava3/Response+Extension.kt
@@ -0,0 +1,29 @@
+package cn.netdiscovery.http.extension.rxjava3
+
+import cn.netdiscovery.http.core.response.ResponseMapper
+import io.reactivex.rxjava3.core.*
+import okhttp3.Response
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.http.extension.rxjava3.`Response+Extension`
+ * @author: Tony Shen
+ * @date: 2021/12/21 11:55 PM
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun <T> Response.asObservable(mapper: ResponseMapper<T>): Observable<T> = Observable.create {
+    it.onNext(mapper.map(this))
+}
+
+fun <T>Response.asFlowable(mapper: ResponseMapper<T>): Flowable<T> = Flowable.create({
+    it.onNext(mapper.map(this))
+}, BackpressureStrategy.BUFFER)
+
+fun <T> Response.asSingle(mapper: ResponseMapper<T>): Single<T> = Single.create {
+    it.onSuccess(mapper.map(this))
+}
+
+fun <T> Response.asMaybe(mapper: ResponseMapper<T>): Maybe<T> = Maybe.create {
+    it.onSuccess(mapper.map(this))
+}
